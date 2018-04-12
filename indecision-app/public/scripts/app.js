17618dd0981f9794d38016fabc468338daa59dbc
Kyle Desktop <kylespresser@gmail.com>
2018-04-12 13:36:29 -0400

    lecture 11

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,16 +2,67 @@
 
 console.log('App.js is running');
 
+var app = {
+  title: 'Indecision App from the object',
+  subtitle: 'This some info from the object'
+};
+
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
-// var template = React.createElement(
-//   "h1",
-//   { id: "someid" },
-//   "This is JSX from app.js"
-// );
+
+var user = {
+  name: 'Jim',
+  age: 5000,
+  location: 'Podunk Island'
+};
+
+var challengeTemplate = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    user.location
+  )
+);
 
 var appRoot = document.getElementById('app');
 
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,11 +1,35 @@
 console.log('App.js is running');
 
-var template = <h1>Indecision App</h1>;
-// var template = React.createElement(
-//   "h1",
-//   { id: "someid" },
-//   "This is JSX from app.js"
-// );
+
+var app = {
+  title: 'Indecision App from the object',
+  subtitle: 'This some info from the object'
+};
+
+var template = (
+  <div>
+    <h1>{app.title}</h1> 
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: 'Jim',
+  age: 5000,
+  location: 'Podunk Island'
+};
+
+var challengeTemplate = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+);
 
 var appRoot = document.getElementById('app');
 
