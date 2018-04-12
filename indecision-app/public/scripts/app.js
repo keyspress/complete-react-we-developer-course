53f440d592272a5b807a5c83f0b6a73b9652cd81
Kyle Desktop <kylespresser@gmail.com>
2018-04-12 12:25:28 -0400

    lecture 8

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,12 @@
+console.log('App.js is running');
+
+// var template = <p>This is JSX from app.js</p>;
+var template = React.createElement(
+  "h1",
+  { id: "someid" },
+  "This is JSX from app.js"
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
