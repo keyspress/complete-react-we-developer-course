a885367757ccc61fea56f1683e87363402fdfd1a
Kyle Desktop <kylespresser@gmail.com>
2018-04-12 22:44:13 -0400

    lecture 16

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -42,40 +42,43 @@ var template = React.createElement(
   )
 );
 
-var user = {
-  name: 'Jim',
-  age: 500,
-  location: 'Podunk Island'
+var count = 0;
+var addOne = function addOne() {
+  console.log('addOne');
+  count = count + 1;
 };
-
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      'Location: ',
-      user.location
-    );
-  }
-}
-
-var challengeTemplate = React.createElement(
+var minusOne = function minusOne() {
+  console.log('minusOne');
+};
+var reset = function reset() {
+  console.log('reset');
+};
+var templateTwo = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    user.name ? user.name : "Anonymous"
+    'Count: ',
+    count
   ),
-  user.age && user.age > 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
   ),
-  getLocation(user.location)
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'Reset'
+  )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
