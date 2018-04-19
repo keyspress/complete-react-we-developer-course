7ab73a02368beca48ec8630840a147432cedb3f4
Kyle Desktop <kylespresser@gmail.com>
2018-04-19 13:23:17 -0400

    lecture 53

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,18 +1,6 @@
-// import './utils.js';
-// import subtractAsDefault, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtractAsDefault(100, 70));
+const template = React.createElement('p', {}, 'testing123');
 
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(18));
-console.log(isAdult(15));
-
-console.log(canDrink(21));
-console.log(canDrink(5));
-
-console.log(isSenior(65));
-console.log(isSenior(3));
+ReactDOM.render(template, document.getElementById('app'));
