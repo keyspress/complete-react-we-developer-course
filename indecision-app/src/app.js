4a96b6ace15f411bd7beb88efe6b75bf21734ebe
Kyle Desktop <kylespresser@gmail.com>
2018-04-20 10:15:17 -0400

    lecture 61 challenge complete

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 
+
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOption';
 import Header from './Header';
 import Action from './Action';
 import Options from './Options';
+import OptionModal from './OptionModal';
 
 export default class IndecisionApp extends React.Component {
   state = {
-  options: []
+  options: [],
+  selectedOption: undefined
   };
 
   handleDeleteOptions = () => {
@@ -20,7 +22,7 @@ export default class IndecisionApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    alert(option);
+    this.setState(() => ({ selectedOption: option}));
   };
   handleAddOption = (option) => {
     if (!option) {
@@ -33,6 +35,9 @@ export default class IndecisionApp extends React.Component {
       options: prevState.options.concat(option)
     }));
   };
+  handleCloseModal = () => {
+    this.setState(() => ({ selectedOption: undefined }));
+  }
   componentDidMount() {
     try {
       const json = localStorage.getItem('options');
@@ -72,6 +77,10 @@ export default class IndecisionApp extends React.Component {
         />
         <AddOption
           handleAddOption={this.handleAddOption}
+        />
+        <OptionModal 
+          selectedOption={this.state.selectedOption}
+          closeModal={this.handleCloseModal}
         />
       </div>
     );
