1ada6e36c0c5cc42fb720246f9d9dbb41b06c2de
Kyle Desktop <kylespresser@gmail.com>
2018-04-23 13:29:02 -0400

    lecture 101

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+  <div>
+    This is the Dashboard
+  </div>
+);
 
+const AddExpensePage = () => (
+  <div>
+    This is the Add Expense Page
+  </div>
+);
 
-ReactDOM.render(<p>Boiler Plate</p>, document.getElementById('app'));
+const EditExpensePage = () => (
+  <div>
+    This is the Edit Expense Page
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is the Help Page
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path='/' component={ExpenseDashboardPage} exact={true} />
+      <Route path='/create' component={AddExpensePage} />
+      <Route path='/edit' component={EditExpensePage} />
+      <Route path='/help' component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
