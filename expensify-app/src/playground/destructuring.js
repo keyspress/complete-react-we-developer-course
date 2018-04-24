50ff60ac1d4da44e504b35b0992978f266b30fb0
Kyle Desktop <kylespresser@gmail.com>
2018-04-24 11:46:52 -0400

    lecture 89

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -17,14 +17,30 @@
 //   console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// };
 
-const { name: publisherName = 'Self-Published'} = book.publisher;
+// const { name: publisherName = 'Self-Published'} = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+//
+// Array desctructuring
+//
+
+const address = ['1299 S Juniper St.', 'Somewhereville', 'Maine', '74464'];
+
+const [ , city, state = 'California' ] = address;
+
+console.log(`You are in ${city} ${state}.`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [ name, , price] = item;
+
+console.log(`A medium ${name} costs ${price}.`);
