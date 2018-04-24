92bc25bf06fa52045cac062d55e26f1d9bca801d
Kyle Desktop <kylespresser@gmail.com>
2018-04-24 11:35:27 -0400

    lecture 88

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,1 +1,30 @@
-console.log('destructuring');
+// const person = {
+//   name: 'George',
+//   age: 800,
+//   location: {
+//     city: 'Somewhere',
+//     temp: 32
+//   }
+// };
+
+// const { name: firstName = 'Anony Mouse', age } = person;
+
+// const { city, temp: temperature } = person.location;
+
+// console.log(`${firstName} is ${age}.`);
+
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}.`);
+// }
+
+const book = {
+  title: 'Ego is the Enemy',
+  author: 'Ryan Holiday',
+  publisher: {
+    name: 'Penguin'
+  }
+};
+
+const { name: publisherName = 'Self-Published'} = book.publisher;
+
+console.log(publisherName);
