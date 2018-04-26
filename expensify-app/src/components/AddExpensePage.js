d41d7253ae7a3c98619f3e7fd2173bdc3feb1ed5
Kyle Desktop <kylespresser@gmail.com>
2018-04-26 13:59:18 -0400

    lecture 105 challenge complete

--- expensify-app/src/components/AddExpensePage.js
+++ expensify-app/src/components/AddExpensePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
   <div>
-    This is the Add Expense Page
+    <h1>Add Expense</h1>
+    <ExpenseForm />
   </div>
 );
 
