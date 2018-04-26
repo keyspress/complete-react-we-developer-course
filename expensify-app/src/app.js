60a91743284949581a0e108ddaa60cf8f474d9a7
Kyle Desktop <kylespresser@gmail.com>
2018-04-26 12:39:22 -0400

    lecture 102 needed guidance on challenge

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -13,12 +14,21 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 200 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 80 }));
-
 store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('bill'));
+}, 3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// console.log(visibleExpenses);
 
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
