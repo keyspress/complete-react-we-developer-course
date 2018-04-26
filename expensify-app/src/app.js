b3e3d43204422a06cc260634f641327de8ca559f
Kyle Desktop <kylespresser@gmail.com>
2018-04-26 10:53:36 -0400

    lecture 99 challenge complete

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import { STATUS_CODES } from 'http';
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill', amount: 200 }));
+store.dispatch(addExpense({ description: 'Gas bill', amount: 80 }));
+
+store.dispatch(setTextFilter('water'));
+
+store.subscribe(() => {
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
+});
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
