7c42c6982fb69a3d7c89c7692be97e3128ecd825
Kyle Desktop <kylespresser@gmail.com>
2018-04-27 09:26:52 -0400

    lecture 107

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -45,6 +45,12 @@ export default class ExpenseForm extends React.Component {
       this.setState(() => ({ error: 'Please provide description and amount' })); 
     } else {
       this.setState(() => ({ error: '' }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
     }
   }
   render() {
