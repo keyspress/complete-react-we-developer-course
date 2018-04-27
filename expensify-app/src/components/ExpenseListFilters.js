360b9ceec6d3389de8d5fe93616430fc228ab4bb
Kyle Desktop <kylespresser@gmail.com>
2018-04-27 11:24:20 -0400

    lecture 110 additional file

--- expensify-app/src/components/ExpenseListFilters.js
+++ expensify-app/src/components/ExpenseListFilters.js
@@ -45,7 +45,7 @@ class ExpenseListFilters extends React.Component {
           onFocusChange={this.onFocusChange}
           showClearDates={true}
           numberOfMonths={1}
-          isOutsideRange={() => false}          
+          isOutsideRange={() => false}
         />
       </div>
     );
