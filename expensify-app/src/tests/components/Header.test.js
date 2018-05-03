e01f6607d6268d01c68037521a836d7efd4e8fe5
Kyle Desktop <kylespresser@gmail.com>
2018-05-03 11:23:03 -0400

    lecture 120 needed help with first challenge, second challenge complete

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
